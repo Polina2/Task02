81a100e095504244f67781b1bad4a7872569152e
Haku <polina231003@mail.ru>
2022-03-21 00:08:03 +0300

    LinkedList methods written

--- src/ru/vsu/cs/aisd/g92/lyigina_p_s/MyLinkedList.java
+++ src/ru/vsu/cs/aisd/g92/lyigina_p_s/MyLinkedList.java
@@ -46,27 +46,68 @@ public class MyLinkedList<T> {
     }
 
     public void addFirst(T value) {
-
+        if (isEmpty()) {
+            head = tail = new ListNode(value);
+        } else {
+            head = new ListNode(value, head);
+        }
+        size++;
     }
 
     public void addLast(T value) {
-
+        if (isEmpty())
+            head = tail = new ListNode(value);
+        else {
+            tail.setNext(new ListNode(value));
+            tail = tail.getNext();
+        }
+        size++;
     }
 
     public void insert(T value, int index) {
-
+        if (index == 0)
+            addFirst(value);
+        else if (index == size)
+            addLast(value);
+        else {
+            ListNode node = getNode(index - 1);
+            node.setNext(new ListNode(value, node.getNext()));
+        }
+        size++;
     }
 
     public void removeFirst() {
-
+        if (isEmpty())
+            throw new IndexOutOfBoundsException("List is empty");
+        if (size == 1) {
+            head = tail = null;
+        } else {
+            head = head.getNext();
+        }
+        size--;
     }
 
     public void removeLast() {
-
+        if (size < 2)
+            removeFirst();
+        else {
+            remove(size-1);
+        }
+        size--;
     }
 
     public void remove(int index) {
-
+        if (index == size)
+            throw new IndexOutOfBoundsException("Index out of bounds");
+        if (index == 0)
+            removeFirst();
+        else {
+            ListNode node = getNode(index - 1);
+            node.setNext(node.getNext().getNext());
+            if (node.getNext() == null)
+                tail = node;
+        }
+        size--;
     }
 
     public boolean isEmpty() {
@@ -74,6 +115,18 @@ public class MyLinkedList<T> {
     }
 
     public T get(int index) {
-        return null;
+        return getNode(index).getValue();
+    }
+
+    private ListNode getNode(int index) {
+        if (index < 0 || index >= size)
+            throw new IndexOutOfBoundsException("Index out of bounds");
+        ListNode current = head;
+        int count = 0;
+        while (count < index) {
+            current = current.getNext();
+            count++;
+        }
+        return current;
     }
 }
